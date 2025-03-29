ba9e2b2107302373e25b4d48099bb08a347725d3
mangila <olsson.erik1993@gmail.com>
2025-03-29 10:22:52 +0100

    network ACL

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -32,46 +32,31 @@ resource "aws_route_table_association" "route_table_association" {
   route_table_id = aws_route_table.pokedex_public_route_table.id
 }
 
-resource "aws_security_group" "pokedex_public_sg" {
-  description = "Public security group"
-  depends_on  = [aws_vpc.pokedex_vpc]
-  vpc_id      = aws_vpc.pokedex_vpc.id
+resource "aws_network_acl" "pokedex_network_acl" {
+  vpc_id = aws_vpc.pokedex_vpc.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "pokedex_public_https_egress" {
-  description       = "Public Internet Egress"
-  security_group_id = aws_security_group.pokedex_public_sg.id
-  ip_protocol       = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_ipv4         = "0.0.0.0/0"
+resource "aws_network_acl_association" "example" {
+  subnet_id      = aws_subnet.pokedex_public_subnet.id
+  network_acl_id = aws_network_acl.pokedex_network_acl.id
 }
 
-resource "aws_subnet" "pokedex_private_subnet" {
-  vpc_id     = aws_vpc.pokedex_vpc.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_network_acl_rule" "allow_https_egress" {
+  network_acl_id = aws_network_acl.pokedex_network_acl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  from_port      = 443
+  to_port        = 443
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
 }
 
-resource "aws_security_group" "pokedex_private_sg" {
-  description = "Private security group"
-  depends_on  = [aws_vpc.pokedex_vpc]
-  vpc_id      = aws_vpc.pokedex_vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "pokedex_private_any_ingress" {
-  description       = "All inbound internal traffic"
-  security_group_id = aws_security_group.pokedex_private_sg.id
-  ip_protocol       = -1
-  from_port         = 0
-  to_port           = 0
-  cidr_ipv4         = aws_subnet.pokedex_private_subnet.cidr_block
-}
-
-resource "aws_vpc_security_group_egress_rule" "pokedex_private_any_egress" {
-  description       = "All outbound internal traffic"
-  security_group_id = aws_security_group.pokedex_private_sg.id
-  ip_protocol       = -1
-  from_port         = 0
-  to_port           = 0
-  cidr_ipv4         = aws_subnet.pokedex_private_subnet.cidr_block
+resource "aws_network_acl_rule" "deny_ingress" {
+  network_acl_id = aws_network_acl.pokedex_network_acl.id
+  rule_action    = "deny"
+  egress         = false
+  protocol       = "-1"
+  cidr_block     = "0.0.0.0/0"
+  rule_number    = 100
 }
