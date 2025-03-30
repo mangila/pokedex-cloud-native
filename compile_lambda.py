0174875055b535f3d24866c3793a61e3712c2791
mangila <olsson.erik1993@gmail.com>
2025-03-31 01:15:28 +0200

    test

--- compile_lambda.py
+++ compile_lambda.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import sys
 
 
 def main():
@@ -14,21 +13,17 @@ def main():
 
 
 def build_go_binaries(lambda_name):
-    try:
-        env = os.environ.copy()
-        env["GOOS"] = "linux"
-        env["GOARCH"] = "amd64"
-        env["CGO_ENABLED "] = "0"
-        go_build_command = "go build -o bootstrap main.go"
-        cwd = f"project/lambda_src/{lambda_name}"
-        subprocess.run(go_build_command,
-                       cwd=cwd,
-                       env=env,
-                       check=True,
-                       shell=True)
-    except Exception as e:
-        print(f"Error while building golang binary for lambda_src -- {lambda_name}: {e}")
-        sys.exit(1)
+    env = os.environ.copy()
+    env["GOOS"] = "linux"
+    env["GOARCH"] = "amd64"
+    env["CGO_ENABLED "] = "0"
+    go_build_command = "go build -o bootstrap main.go"
+    cwd = f"project/lambda_src/{lambda_name}"
+    subprocess.run(go_build_command,
+                   cwd=cwd,
+                   env=env,
+                   check=True,
+                   shell=True)
 
 
 if __name__ == "__main__":
