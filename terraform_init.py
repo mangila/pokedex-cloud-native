1ad4c7a74e96e13270d31e9248b0b785aa315686
mangila <olsson.erik1993@gmail.com>
2025-03-30 23:27:52 +0200

    test

---
+++ terraform_init.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+
+
+def main():
+    """run terraform `init`"""
+    os.chdir("infrastructure")
+    terraform_format()
+
+
+def terraform_format():
+    tf_command = "terraform init"
+    print(tf_command)
+    out = subprocess.run(tf_command, check=True, shell=True)
+    if out.returncode != 0:
+        sys.exit(out.returncode)
+
+
+if __name__ == "__main__":
+    main()
