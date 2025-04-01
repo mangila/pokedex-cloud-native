a8519e4d527ba66203d153a969b376a4ee95a47c
mangila <olsson.erik1993@gmail.com>
2025-04-02 00:02:43 +0200

    tf modules - remove fmt check in ci - pre-commit instead

--- terraform_plan.py
+++ terraform_plan.py
@@ -1,15 +1,18 @@
+import os
 import subprocess
 
 from terraform_vars import ENV_TF_VARS_FILE, SECRET_TF_VARS_FILE, TF_PLAN_FILE
 
 
 def terraform_plan():
-    tf_command = f"terraform plan -var-file={ENV_TF_VARS_FILE} -var-file={SECRET_TF_VARS_FILE} -out={TF_PLAN_FILE}"
+    tf_command = f"terraform plan -var-file={ENV_TF_VARS_FILE} -out={TF_PLAN_FILE}"
+    if os.path.isfile(SECRET_TF_VARS_FILE):
+        print(f"Including {SECRET_TF_VARS_FILE} in the command.")
+        tf_command += f" -var-file={SECRET_TF_VARS_FILE}"
+    else:
+        print(f"Warning: {SECRET_TF_VARS_FILE} not found. Running without it.")
     print(tf_command)
-    subprocess.run(tf_command,
-                   cwd="app",
-                   check=True,
-                   shell=True)
+    subprocess.run(tf_command, cwd="app", check=True, shell=True)
 
 
 if __name__ == "__main__":
