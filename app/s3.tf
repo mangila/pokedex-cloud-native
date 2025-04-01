301ac5fc78dc46b582469d6fcae7befcb745d66c
mangila <olsson.erik1993@gmail.com>
2025-04-01 16:20:58 +0200

    lambda test

--- app/s3.tf
+++ app/s3.tf
@@ -21,8 +21,8 @@ data "archive_file" "hello_zip" {
 }
 
 resource "aws_s3_object" "hello_s3_object" {
-  bucket = aws_s3_bucket.pokedex_lambda_bucket.id
-  key    = "hello-bootstrap.zip"
-  source = data.archive_file.hello_zip.output_path
-  etag   = data.archive_file.hello_zip.output_base64sha256
+  bucket      = aws_s3_bucket.pokedex_lambda_bucket.id
+  key         = "hello-bootstrap.zip"
+  source      = data.archive_file.hello_zip.output_path
+  source_hash = data.archive_file.hello_zip.output_base64sha256
 }
