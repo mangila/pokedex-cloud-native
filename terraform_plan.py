b68d3f11b2a2c3725507d83770f0286b85d0fba3
mangila <olsson.erik1993@gmail.com>
2025-04-01 12:19:58 +0200

    tf plan

--- terraform_plan.py
+++ terraform_plan.py
@@ -1,10 +1,10 @@
 import subprocess
 
-from terraform_vars import ENV_TF_VARS_FILE, SECRET_TF_VARS_FILE
+from terraform_vars import ENV_TF_VARS_FILE, SECRET_TF_VARS_FILE, TF_PLAN_FILE
 
 
 def terraform_plan():
-    tf_command = f"terraform plan -var-file={ENV_TF_VARS_FILE} -var-file={SECRET_TF_VARS_FILE}"
+    tf_command = f"terraform plan -var-file={ENV_TF_VARS_FILE} -var-file={SECRET_TF_VARS_FILE} -out={TF_PLAN_FILE}"
     print(tf_command)
     subprocess.run(tf_command,
                    cwd="app",
