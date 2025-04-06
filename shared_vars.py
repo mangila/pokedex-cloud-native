9e4ec6240fa161c0630fada22ca33c22cea7f40c
mangila <olsson.erik1993@gmail.com>
2025-04-06 19:13:41 +0200

    v2

--- shared_vars.py
+++ shared_vars.py
@@ -1,14 +1,2 @@
-ENV_TF_VARS_FILE = "vars/dev.tfvars.tf"
 SECRET_TF_VARS_FILE = "vars/secret.tfvars.tf"
 TF_PLAN_FILE = "pokedex-tfplan"
-LAMBDAS = [
-    {"name": "enrich_pokemon"},
-    {"name": "fetch_generation"},
-    {"name": "persist_pokemon"},
-]
-GO_MODULES = [
-    {"name": "enrich_pokemon"},
-    {"name": "fetch_generation"},
-    {"name": "persist_pokemon"},
-    {"name": "shared"},
-]
